022f5055f5fed4a95a8a17e9ecc9e0359fd008bc
jaybk051183 <kailashbhaggoe@outlook.com>
2022-09-16 22:00:42 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -5,16 +5,38 @@ import CavitiesPage from './pages/Cavities';
 import AppointmentsPage from './pages/Appointments';
 import WhiteningPage from './pages/Whitening';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <>
+    <Router>
       <TopMenu />
+
+        <Switch>
+            <Route exact path="/">
       <HomePage />
+            </Route>
+
+            <Route path="/gaatjes">
       <CavitiesPage />
+            </Route>
+
+            <Route path="/afspraak-maken">
       <AppointmentsPage />
+            </Route>
+
+            <Route path="/tanden-bleken">
       <WhiteningPage />
-    </>
+            </Route>
+
+        </Switch>
+    </Router>
   );
 }
 
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 function TopMenu() {
   return (
@@ -8,19 +9,19 @@ function TopMenu() {
 
         <ul>
           <li>
-            Home
+            <NavLink to="/" activeClassName="active-link">Home</NavLink>
           </li>
 
           <li>
-            Gaatjes
+            <NavLink to="/gaatjes" activeClassName="active-link">Gaatjes</NavLink>
           </li>
 
           <li>
-            Afspraak maken
+            <NavLink to="/afspraak-maken" activeClassName="active-link">Afspraak maken</NavLink>
           </li>
 
           <li>
-            Tanden bleken
+            <NavLink to="/tanden-bleken" activeClassName="active-link">Tanden bleken</NavLink>
           </li>
         </ul>
       </div>
